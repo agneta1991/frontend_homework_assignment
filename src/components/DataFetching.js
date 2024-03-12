c15e3f3d24261410b1373815d1641a7da0bd7264
agneta1991 <pupienytea@gmail.com>
2024-03-12 09:52:46 +0200

    Local storrage set up to favor the images

--- src/components/DataFetching.js
+++ src/components/DataFetching.js
@@ -1,4 +1,5 @@
 /* eslint-disable */
+
 import React, { useState, useEffect } from "react";
 import { createClient } from "pexels";
 import "./style.css";
@@ -9,6 +10,7 @@ const client = createClient(
 
 const DisplayData = () => {
   const [photos, setPhotos] = useState([]);
+  const [favorites, setFavorites] = useState([]);
 
   useEffect(() => {
     const fetchPhotos = async () => {
@@ -23,26 +25,57 @@ const DisplayData = () => {
     fetchPhotos();
   }, []);
 
+  useEffect(() => {
+    const storedFavorites = JSON.parse(localStorage.getItem("favorites")) || [];
+    setFavorites(storedFavorites);
+  }, []);
+
+  const handleFavoriteClick = (id) => {
+    const isFavorite = favorites.includes(id);
+
+    const updatedFavorites = isFavorite
+      ? favorites.filter((favoriteId) => favoriteId !== id)
+      : [...favorites, id];
+
+    localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
+    setFavorites(updatedFavorites);
+  };
+
   function capitaliseName(name) {
     const fullName = name.split(" ");
-    const capitaliseFullName = fullName.map((fullName) => {
-      return fullName.charAt(0).toUpperCase() + fullName.slice(1);
-    });
+    const capitaliseFullName = fullName.map(
+      (fullName) => fullName.charAt(0).toUpperCase() + fullName.slice(1)
+    );
 
     return capitaliseFullName.join(" ");
   }
 
   return (
-    <div class="photo-div">
+    <div className="photo-div">
       {photos.map((photo) => (
-        <div key={photo.id} class="individual-photo-div">
-          <img class="fetched-photo" src={photo.src.medium} alt={photo.alt} />
-          <div class="overlay">
-            <p class="image-name">"{capitaliseName(photo.alt)}"</p>
-            <span></span>
-            <p class="photographer">{capitaliseName(photo.photographer)}</p>
+        <div
+          key={photo.id}
+          className={`individual-photo-div ${
+            favorites.includes(photo.id) ? "favored" : ""
+          }`}
+        >
+          <img
+            className="fetched-photo"
+            src={photo.src.medium}
+            alt={photo.alt}
+          />
+          <div className="overlay">
+            <p className="image-name">{capitaliseName(photo.alt)}</p>
+            <span />
+            <p className="photographer">{capitaliseName(photo.photographer)}</p>
 
-            <button>Favorite</button>
+            <button
+              type="submit"
+              className="favorite"
+              onClick={() => handleFavoriteClick(photo.id)}
+            >
+              Favorite
+            </button>
           </div>
         </div>
       ))}
